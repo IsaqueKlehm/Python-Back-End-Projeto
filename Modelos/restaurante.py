77c9ef9eef134dc0d954ba086aaad9ae3fe66888
Isaque Klehm <isaqueklehm@gmail.com>
2026-08-27 17:41:40 -0300

    Update 2026-08-27

--- Modelos/restaurante.py
+++ Modelos/restaurante.py
@@ -1,17 +1,31 @@
 class Restaurante():
+    restaurantes = []
 
-    def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+    def __init__(self, nome, categoria, ativo):
+        self._nome = nome.title()
+        self._categoria = categoria.title()
+        self._ativo = ativo
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria} | {self.ativo}'
+        return f'{self._nome} | {self._categoria} | {self.ativo}'
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'\n{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}\n')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.status}')
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+    @property
+    def status(self):
+        return '🟢 Aberto' if self._ativo else '🔴 Fechado'
 
-restaurentes = [restaurante_praca, restaurante_pizza]
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+    
 
-print(restaurante_praca)
-print(restaurante_pizza)
+restaurante_praca = Restaurante('praça', 'Gourmet', False)
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza express', 'Italiana', True)
+
+Restaurante.listar_restaurantes()
