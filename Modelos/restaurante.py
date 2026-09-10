c70f04aefe048e104ab21b9260e3f6894fe0999c
Isaque Klehm <isaqueklehm@gmail.com>
2026-09-10 19:23:13 -0300

    Update: Projeto Sabor Express implementado herança

--- Modelos/restaurante.py
+++ Modelos/restaurante.py
@@ -1,10 +1,13 @@
+from Modelos.avaliacao import Avaliacao
+
 class Restaurante():
     restaurantes = []
 
-    def __init__(self, nome, categoria, ativo):
+    def __init__(self, nome, categoria):
         self._nome = nome.title()
         self._categoria = categoria.title()
-        self._ativo = ativo
+        self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -12,9 +15,9 @@ class Restaurante():
     
     @classmethod
     def listar_restaurantes(cls):
-        print(f'\n{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}\n')
+        print(f'\n{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}\n')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.status}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} |{str(restaurante.media_avaliacoes).ljust(24)} | {restaurante.status}')
 
     @property
     def status(self):
@@ -22,10 +25,19 @@ class Restaurante():
 
     def alternar_estado(self):
         self._ativo = not self._ativo
+
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas/quantidade_de_notas,1)
+        return media
+
+
     
-
-restaurante_praca = Restaurante('praça', 'Gourmet', False)
-restaurante_praca.alternar_estado()
-restaurante_pizza = Restaurante('pizza express', 'Italiana', True)
-
-Restaurante.listar_restaurantes()
---
+++ app.py
@@ -0,0 +1,18 @@
+from Modelos.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_mexicano = Restaurante('mexican food', 'Mexicana')
+restaurante_japones = Restaurante('japa', 'Japonesa')
+# restaurante_mexicano.alternar_estado()
+# restaurante_japones.alternar_estado()
+
+restaurante_praca.receber_avaliacao('Gui', 10)
+restaurante_praca.receber_avaliacao('Lais', 8)
+restaurante_praca.receber_avaliacao('Emy', 2)
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if  __name__ == '__main__':
+    main()
